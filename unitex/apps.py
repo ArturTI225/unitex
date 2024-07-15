96d96f7117bd4240da74d013aa64573ec45a3f3c
Delewer <130284049+Delewer@users.noreply.github.com>
2024-07-15 17:23:53 +0300

    Initial commit

---
+++ unitex/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UnitexConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'unitex'
---
+++ unitex/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegisterForm
+
+def index(request):
+    return render(request, 'unitex/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegisterForm()
+    return render(request, 'unitex/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'unitex/login.html', {'form': form})
